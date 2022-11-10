9b539453a1d755f9b649dd450530dedd59638647
daniel tenorio <juantenorio16@augustana.edu>
2022-11-10 17:29:26 -0600

    php change

--- php/feedback.php
+++ php/feedback.php
@@ -21,17 +21,17 @@
     $sql = "INSERT INTO t_feedback VALUES
         ('$name', '$email', '$comment')";
     
-    if(mysqli_query($conn, $sql)){
-        echo "<h3>data stored in a database successfully."
-            . " Please browse your localhost php my admin"
-            . " to view the updated data</h3>";
+    // if(mysqli_query($conn, $sql)){
+    //     echo "<h3>data stored in a database successfully."
+    //         . " Please browse your localhost php my admin"
+    //         . " to view the updated data</h3>";
 
-        echo nl2br("\n$first_name\n $last_name\n "
-            . "$gender\n $address\n $email");
-    } else{
-        echo "ERROR: Hush! Sorry $sql. "
-            . mysqli_error($conn);
-    }
+    //     echo nl2br("\n$first_name\n $last_name\n "
+    //         . "$gender\n $address\n $email");
+    // } else{
+    //     echo "ERROR: Hush! Sorry $sql. "
+    //         . mysqli_error($conn);
+    // }
      
     // Close connection
     mysqli_close($conn);
