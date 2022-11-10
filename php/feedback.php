fb218aacbe54be3791cdcd9236638ccfe0779cc4
daniel tenorio <juantenorio16@augustana.edu>
2022-11-10 13:42:05 -0600

    Added form validation and php

---
+++ php/feedback.php
@@ -0,0 +1,7 @@
+<?php
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $comment = htmlspecialchars($_POST['comment']);
+    
+    echo 'Name: ' + $name + ', Email: ' + $email + ', Comment: ' + $comment;
+?>
