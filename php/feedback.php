7b5a828d7c739854b207019d2ca4212dfb156745
daniel tenorio <juantenorio16@augustana.edu>
2022-11-10 17:26:53 -0600

    readded php

--- php/feedback.php
+++ php/feedback.php
@@ -1,33 +1,42 @@
 <?php
-    // $name = htmlspecialchars($_POST['name']);
-    // $email = htmlspecialchars($_POST['email']);
-    // $comment = htmlspecialchars($_POST['comment']);
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $comment = htmlspecialchars($_POST['comment']);
 
-    // $servername = "us-phx-web737.main-hosting.eu";
-    // $username = "u668543051_JDT";
-    // $password = "MSin2m!!";
-    // $dbname = "u668543051_JDTMySQL";
+    $servername = "us-phx-web737.main-hosting.eu";
+    $username = "u668543051_JDT";
+    $password = "MSin2m!!";
+    $dbname = "u668543051_JDTMySQL";
     
-    // // Create connection
-    // $conn = new mysqli($servername,
-    //     $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername,
+        $username, $password, $dbname);
     
-    // // Check connection
-    // if ($conn->connect_error) {
-    //     die("Connection failed: "
-    //         . $conn->connect_error);
-    // }
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: "
+            . $conn->connect_error);
+    }
     
-    // $sqlquery = "INSERT INTO t_feedback VALUES
-    //     ('$name', '$email', '$comment')";
+    $sql = "INSERT INTO t_feedback VALUES
+        ('$name', '$email', '$comment')";
     
-    // if ($conn->query($sql) === TRUE) {
-    //     echo "record inserted successfully";
-    // } else {
-    //     echo "Error: " . $sql . "<br>" . $conn->error;
-    // }
-    
-    // mysqli_close($conn);
+    if(mysqli_query($conn, $sql)){
+        echo "<h3>data stored in a database successfully."
+            . " Please browse your localhost php my admin"
+            . " to view the updated data</h3>";
 
-    // echo 'Name: ' . $name . ', Email: ' . $email . ', Comment: ' . $comment;
+        echo nl2br("\n$first_name\n $last_name\n "
+            . "$gender\n $address\n $email");
+    } else{
+        echo "ERROR: Hush! Sorry $sql. "
+            . mysqli_error($conn);
+    }
+     
+    // Close connection
+    mysqli_close($conn);
+    
+    mysqli_close($conn);
+
+    echo 'Name: ' . $name . ', Email: ' . $email . ', Comment: ' . $comment;
 ?>
